f0a71a4b0876a130582e8b7f67699c5c91380ff4
vasutata1111 <thathavasu7@gmail.com>
2019-09-25 23:17:26 +0530

    set to dict

---
+++ prg1.py
@@ -0,0 +1,8 @@
+# Write a Python program to match key values in two dictionaries.
+#Sample dictionary: {'key1': 1, 'key2': 3, 'key3': 2}, {'key1': 1, 'key2': 2}
+#Expected output: key1: 1 is present in both x and y
+x = {'key1': 1, 'key2': 3, 'key3': 2}
+y = {'key1': 1, 'key2': 2}
+s1 = set(x.items())
+s2 = set(y.items())
+print(s1 & s2)
